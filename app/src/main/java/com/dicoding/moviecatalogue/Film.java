4153388492541f442f100e414714dbcbcb2cdc91
adit <aditya_hermansyahputra@yahoo.com>
2019-06-23 02:51:09 +0700

    FilmAdapter

---
+++ app/src/main/java/com/dicoding/moviecatalogue/Film.java
@@ -0,0 +1,32 @@
+package com.dicoding.moviecatalogue;
+
+public class Film {
+    private int poster;
+    private String title;
+
+    public int getPoster() {
+        return poster;
+    }
+
+    public void setPoster(int poster) {
+        this.poster = poster;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    private String description;
+}
